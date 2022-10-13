40776298c0d7c9c26017152ee0bda9732f0a1a35
Nathan S. Sodolak <stu53894@bisd.org>
2022-10-13 17:08:13 -0500

    Create Flask App

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def cats():
-    return {"message": "cats!"}
+    url = "https://api.thecatapi.com/v1/images/search"
+    r = requests.get(url)
+    json = r.json()
+    url = json[0]["url"]
+
+    return "<img src = '" + url + "'></img>"
+
+# --------------------------------------------------
+
+# import requests
+
+# url = "https://api.thecatapi.com/v1/images/search"
+# r = requests.get(url)
+# json = r.json()
+# url = json[0]["url"]
+# print(url)
+# <img scr="cat.com/catPic.jpg"></img>
